18cbf7ec26d5bda17a4af48d176ce588c405715e
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-09-15 07:30:54 +0300

    Log out

--- app/src/main/java/com/naburi/workoutlog/ui/HomeActivity.kt
+++ app/src/main/java/com/naburi/workoutlog/ui/HomeActivity.kt
@@ -1,16 +1,32 @@
 package com.naburi.workoutlog.ui
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.naburi.workoutlog.R
 import com.naburi.workoutlog.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     lateinit var binding: ActivityHomeBinding
+    lateinit var sharePrefs: SharedPreferences
+    lateinit var tvLogout: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        tvLogout=findViewById(R.id.tvLogout)
+        tvLogout. setOnClickListener {
+            val editor = sharePrefs.edit()
+            editor.putString("ACCESS_TOKEN", "")
+            editor.putString("USER_ID", "")
+            editor.putString("PROFILE_ID", "")
+            editor.apply()
+            startActivity(Intent(this, LoginActivity::class.java))
+            logoutRequest()
+        }
 
         setupBottomNav()
 
@@ -37,5 +53,9 @@ class HomeActivity : AppCompatActivity() {
                 else->false
             }
         }
+    }
+    fun logoutRequest(){
+        sharePrefs.edit().clear().commit()
+
     }
 }
